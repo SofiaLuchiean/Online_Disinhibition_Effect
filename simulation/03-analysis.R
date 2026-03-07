86e0d04451105a4b56e91ba047a6731430d63133
SofiaLuchiean <mailsforsofia@gmail.com>
2026-03-07 18:12:49 +0100

    new analyses: hypothesis tests, effect sizes and CIs

--- simulation/03-analysis.R
+++ simulation/03-analysis.R
@@ -1,25 +1,50 @@
 source("simulation/02-simulations.R")
 
-install.packages("multcomp")
 library(multcomp)
+library(MBESS)
 
 
-## Wir haben einen fit erstellt
-fit_lm <- lm(bad_sentence_percentage ~ anonymity * cues, data=df)
+# fit the linear model
+fit_lm <- lm(bad_sentence_percentage ~ MOD + anonymity * cues, data=df)
 summary(fit_lm)
 
-## Nullhypothesen definieren (Richtung kann man nicht mischen, deshalb getrennt)
-hyps_less <- c("anonymity <=0")
-hyp_greater <- c("cues >= 0", "anonymity:cues >= 0")
 
-## glht Objekt erstellen 
-# für Nullhypothesen kleiner gleich
+# test hypotheses
+## define null hypotheses 
+hyps_less <- c("anonymity <= 0", "MOD <= 0")
+hyps_greater <- c("cues >= 0", "anonymity:cues >= 0")
+
+## glht objects 
+### null hypotheses <= 0
 fit_glht_less <- glht(fit_lm, hyps_less)
-summary(fit_glht_less)
 
-# für Nullhypothesen größer gleich
-fit_glht_greater <- glht(fit_lm, hyp_greater)
-summary(fit_glht_greater)
+### null hypotheses >= 0
+fit_glht_greater <- glht(fit_lm, hyps_greater)
+
+## results
+summary(fit_glht_less, test=univariate())
+summary(fit_glht_greater, test=univariate())
 
 
-summary(fit_lm)
+# effect sizes and their 95% CIs
+## R^2
+summary_fit <- summary(fit_lm)
+R2 <- round(summary_fit$r.squared, 2)
+ci.R2(R2 = R2, p = 4, N = nrow(df)) 
+
+## beta_zj 
+fit_z <- lm(scale(bad_sentence_percentage) ~ scale(MOD) + scale(anonymity) * scale(cues), data = df)
+summary_fit_z <- summary(fit_z)
+bz_MOD <- round(summary_fit_z$coefficients[2,1],2)
+bz_anonymity <- round(summary_fit_z$coefficients[3,1],2)
+bz_cues <- round(summary_fit_z$coefficients[4,1],2)
+bz_interaction <- round(summary_fit_z$coefficients[5,1],2)
+confint(fit_z)
+
+
+# 
+
+
+
+
+
