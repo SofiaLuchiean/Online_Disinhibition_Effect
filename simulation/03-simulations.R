c02b1d0a6ccde654a1eb460e6637794e4596a4a9
SofiaLuchiean <mailsforsofia@gmail.com>
2026-03-07 19:17:02 +0100

    renamed

--- simulation/03-simulations.R
+++ simulation/03-simulations.R
@@ -2,7 +2,7 @@
 source("simulation/01-functions.R")
 set.seed(41)
 
-n = 100
+n = 10
 df <- expand.grid(
   id = 1:n,
   anonymity = c(0, 0.5, 1),
@@ -14,7 +14,6 @@ df$MOD <- round((rbeta(nrow(df), 1.6, 1.7)*4 + 1), 2) #simulated MOD values
 df$base_resp <- round(rbeta(nrow(df), 6.6, 1.38), 2) #simulated base responsibility values
 
 df$bad_sentence_percentage <- round(curse_function(df$anonymity, df$cues, df$MOD, df$base_resp),2) #simulated disinhibited behavior
-
 
 # 2. plots with simulated data
 p1 = ggplot(df, aes(x= anonymity, y = bad_sentence_percentage, color = factor(cues))) +
