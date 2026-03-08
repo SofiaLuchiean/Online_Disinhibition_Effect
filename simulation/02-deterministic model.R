d10992eaf86fddac6f603b58e9c38078ed0ecc52
SofiaLuchiean <mailsforsofia@gmail.com>
2026-03-08 12:54:15 +0100

    minor changes

--- simulation/02-deterministic model.R
+++ simulation/02-deterministic model.R
@@ -5,13 +5,13 @@ library(ggplot2)
 ## define levels to be plotted
 plot_data <- expand.grid(
   anonymity = c(0, 0.25, 0.5, 0.75, 1), #experimental factor
-  MOD = c(1, 3, 5),         #measured trait variable
-  cues = c(0, 0.5, 1),      #experimental factor
-  base_resp = c(0.5, 0.9)   #measured trait variable
+  MOD = c(1, 3, 5),                     #measured trait variable
+  cues = c(0, 0.5, 1),                  #experimental factor
+  base_resp = c(0.5, 0.9)               #measured trait variable
 )
 
 ## compute outcome variable
-plot_data$bad_sentence_percentage <- psi_function(
+plot_data$state_disinhibition <- psi_function(
   plot_data$anonymity, 
   plot_data$cues, 
   plot_data$MOD, 
@@ -19,12 +19,12 @@ plot_data$bad_sentence_percentage <- psi_function(
   )
 
 # plot
-ggplot(plot_data, aes(x= anonymity, y = bad_sentence_percentage, color = factor(cues))) +
+ggplot(plot_data, aes(x= anonymity, y = state_disinhibition, color = factor(cues))) +
   facet_grid(base_resp ~ MOD) +
   geom_line() +
   labs(
     x="Anonymity",
-    y="Bad sentence percentage",
+    y="State Online Disinhibition",
     color="Interpersonal cues"
   ) +
   theme_bw()
