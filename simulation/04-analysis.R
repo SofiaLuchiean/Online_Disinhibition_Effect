050d55f87bf38abee07d342fd1255d166c6f52fd
SofiaLuchiean <mailsforsofia@gmail.com>
2026-03-08 16:25:20 +0100

    removed MOD from analyses and tested again corrected alpha-level for multiple testing

--- simulation/04-analysis.R
+++ simulation/04-analysis.R
@@ -5,13 +5,22 @@ library(MBESS)
 
 
 # fit the linear model
-fit_lm <- lm(bad_sentence_percentage ~ MOD + anonymity * cues, data=df)
+fit_lm <- lm(bad_sentence_percentage ~ anonymity * cues, data=df)
 summary(fit_lm)
 
 
 # test hypotheses
+## note: the overall hypothesis is a compound hypothesis, confirmed if 
+## at least one of its constituent hypotheses is supported. Therefore,
+## the alpha level needs to be adjusted to account for this multiple testing.
+alpha <- 0.05
+N <- 3   # as there is three individual hypotheses being tested
+alpha_adj <- alpha / N
+print(alpha_adj) # bonferroni-corrected alpha level
+
+
 ## define null hypotheses 
-hyps_less <- c("anonymity <= 0", "MOD <= 0")
+hyps_less <- c("anonymity <= 0")
 hyps_greater <- c("cues >= 0", "anonymity:cues >= 0")
 
 ## glht objects 
@@ -22,6 +31,7 @@ fit_glht_less <- glht(fit_lm, hyps_less)
 fit_glht_greater <- glht(fit_lm, hyps_greater)
 
 ## results
+### use adjusted alpha here (alpha_adj)
 summary(fit_glht_less, test=univariate())
 summary(fit_glht_greater, test=univariate())
 
@@ -30,15 +40,18 @@ summary(fit_glht_greater, test=univariate())
 ## R^2
 summary_fit <- summary(fit_lm)
 R2 <- round(summary_fit$r.squared, 2)
-ci.R2(R2 = R2, p = 4, N = nrow(df)) 
+print(R2)
+ci.R2(R2 = R2, p = 3, N = nrow(df)) 
 
 ## beta_zj 
-fit_z <- lm(scale(bad_sentence_percentage) ~ scale(MOD) + scale(anonymity) * scale(cues), data = df)
+fit_z <- lm(scale(bad_sentence_percentage) ~ scale(anonymity) * scale(cues), data = df)
 summary_fit_z <- summary(fit_z)
-bz_MOD <- round(summary_fit_z$coefficients[2,1],2)
-bz_anonymity <- round(summary_fit_z$coefficients[3,1],2)
-bz_cues <- round(summary_fit_z$coefficients[4,1],2)
-bz_interaction <- round(summary_fit_z$coefficients[5,1],2)
+bz_anonymity <- round(summary_fit_z$coefficients[2,1],2)
+print(bz_anonymity)
+bz_cues <- round(summary_fit_z$coefficients[3,1],2)
+print(bz_cues)
+bz_interaction <- round(summary_fit_z$coefficients[4,1],2)
+print(bz_interaction)
 confint(fit_z)
 
 
