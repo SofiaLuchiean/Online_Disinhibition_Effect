782cba0d38407da586026ed394cdf1e3ec36a248
SofiaLuchiean <mailsforsofia@gmail.com>
2026-03-13 19:52:00 +0100

    plots

--- simulation/02-atomic functions and combined model.R
+++ simulation/02-atomic functions and combined model.R
@@ -67,7 +67,7 @@ dev.off()
 # plot of the combined model output: state disinhibition
 ## define levels to be plotted
 plot_data <- expand.grid(
-  anonymity = c(0, 0.25, 0.5, 0.75, 1), #experimental factor
+  anonymity = seq(0, 1, by = 0.1),      #experimental factor
   MOD = c(1, 3, 5),                     #measured trait variable
   cues = c(0, 0.5, 1),                  #experimental factor
   base_resp = c(0.5, 0.9)               #measured trait variable
@@ -90,8 +90,12 @@ ggplot(plot_data, aes(x= anonymity, y = state_disinhibition, color = factor(cues
     y="State Online Disinhibition",
     color="Interpersonal cues"
   ) +
-  theme_bw()
-
+  theme_minimal() +
+  theme(
+    axis.text.x  = element_text(size = 7),   
+    axis.text.y  = element_text(size = 7)    
+  )
+ggsave("simulation/plots/model_output.png")
 
 
 
